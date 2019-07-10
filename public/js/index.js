bfa13927d5bc25d9dc9fb18208612a6d23357307
Muniralsharif <munir.sniper@gmail.com>
2019-07-10 14:55:42 +0300

    css modifiyng

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+//  
 const searchBar = document.getElementById("searchBar");
 searchBar.addEventListener("keyup", () => {
   let inputValue = searchBar.value.toLowerCase();
@@ -6,37 +7,49 @@ searchBar.addEventListener("keyup", () => {
     fetch(`/search/${inputValue}`)
       .then(response => response.json())
       .then(data => {
-		  
-	    
-		document.getElementById("list").textContent = "";
-	for (var i=0;i<5;i++) { 
-		var node = document.createElement("LI");        
-		var textnode = document.createTextNode(`${data.filteredData[i]}`);  
-                	      
-		node.appendChild(textnode);   
-		if(data.filteredData[i] !== undefined){                            
-		document.getElementById("list").appendChild(node); 
-		}
-	} 
-		
+        document.getElementById("list").textContent = "";
+        for (var i = 0; i < 5; i++) {
+          var node = document.createElement("LI");
+          var textnode = document.createTextNode(`${data.filteredData[i]}`);
 
-	})
+          node.appendChild(textnode);
+          if (data.filteredData[i] !== undefined) {
+            document.getElementById("list").appendChild(node);
+          }
+        }
+	  }
+	  
+	  )
       .catch(error => console.log(error));
-  }else {
-
-	document.getElementById("list").textContent = "";
-
+  } else {
+    document.getElementById("list").textContent = "";
   }
 });
 
-// const request = ()=>{
-// 	document.getElementById("list").textContent = "";
-// 	for (var i=0;i<5;i++) { 
-// 		var node = document.createElement("LI");        
-// 		var textnode = document.createTextNode(`${data.filteredData[i]}`);  
-                	      
-// 		node.appendChild(textnode);                               
-// 		document.getElementById("list").appendChild(node); 
-		
-// 	} 
-// }
+const request = (url,cb)=>{
+	fetch(url)
+	.then(response => response.json())
+	.then(data => cb(data))
+	.catch(error => error)
+}
+
+const search = document.getElementById("search");
+search.addEventListener("click", ()=>{
+	document.getElementById("list").innerText = "";
+	const div = document.querySelector("#images");
+  div.textContent = "";
+	
+	request("http://api.giphy.com/v1/gifs/search?q=" +
+	searchBar.value +
+	"&api_key=p0FMwmf9fytJSZw4DQgJq6Vurt4S8oHv&limit=4", (result)=>{
+		console.log(result.data);
+		result.data.forEach(element => {
+			const img = document.createElement("img");
+			img.src = `https://media.giphy.com/media/${element.id}/giphy.gif`;
+			div.appendChild(img);
+			
+		});
+}
+)
+});
+
